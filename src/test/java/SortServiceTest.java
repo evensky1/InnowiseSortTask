8d68e95124eb7f2b9163f6baa97d58e246da75f1
evensky1 <fromthedeepsize@gmail.com>
2023-03-07 10:12:21 +0300

    Current array initialization was replaced with Arrays.asList()

--- src/test/java/SortServiceTest.java
+++ src/test/java/SortServiceTest.java
@@ -1,5 +1,4 @@
 import com.innowise.internship.comparators.BallSizeComparator;
-import com.innowise.internship.comparators.BallSizeDescComparator;
 import com.innowise.internship.model.Ball;
 import com.innowise.internship.model.Color;
 import com.innowise.internship.model.impl.BaseBall;
@@ -10,6 +9,7 @@ import com.innowise.internship.sort.SortService;
 import com.innowise.internship.sort.impl.OddEvenSortServiceImpl;
 import com.innowise.internship.sort.impl.QuickSortServiceImpl;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Assertions;
@@ -28,13 +28,13 @@ class SortServiceTest {
     @ParameterizedTest
     @MethodSource("sortServiceStream")
     void oddCountElementsTestDefaultScenario(SortService sortService) {
-        var actual = new ArrayList<>(List.of(new Ball[]{
+        var actual = Arrays.asList(
             new BaseBall(6, "Demix", Color.BLACK),
             new FootBall(2, "Anta", Color.GREEN),
             new BasketBall(4, "Nike", Color.RED),
             new FootBall(6, "Nike", Color.GREEN),
-            new BaseBall(12, "Adidas", Color.BLUE)
-        }));
+            new BaseBall(12, "Adidas", Color.BLUE));
+
 
         var expected = new ArrayList<>(actual);
 
@@ -47,14 +47,13 @@ class SortServiceTest {
     @ParameterizedTest
     @MethodSource("sortServiceStream")
     void evenCountElementsTestDefaultScenario(SortService sortService) {
-        var actual = new ArrayList<>(List.of(new Ball[]{
+        var actual = Arrays.asList(
             new BaseBall(6, "Demix", Color.BLACK),
             new FootBall(2, "Anta", Color.GREEN),
             new BasketBall(4, "Nike", Color.RED),
             new FootBall(6, "Nike", Color.GREEN),
             new BaseBall(12, "Adidas", Color.BLUE),
-            new TennisBall(32, "Nike", Color.RED)
-        }));
+            new TennisBall(32, "Nike", Color.RED));
 
         var expected = new ArrayList<>(actual);
 
@@ -90,13 +89,12 @@ class SortServiceTest {
     @ParameterizedTest
     @MethodSource("sortServiceStream")
     void reversedArrayTest(SortService sortService) {
-        var actual = new ArrayList<>(List.of(new Ball[]{
+        var actual = Arrays.asList(
             new BaseBall(12, "Demix", Color.BLACK),
             new FootBall(7, "Anta", Color.GREEN),
             new BasketBall(6, "Nike", Color.RED),
             new FootBall(4, "Nike", Color.GREEN),
-            new BaseBall(2, "Adidas", Color.BLUE)
-        }));
+            new BaseBall(2, "Adidas", Color.BLUE));
 
         var expected = new ArrayList<>(actual);
 
@@ -109,10 +107,9 @@ class SortServiceTest {
     @ParameterizedTest
     @MethodSource("sortServiceStream")
     void twoElementArrayTest(SortService sortService) {
-        var actual = new ArrayList<>(List.of(new Ball[]{
+        var actual = Arrays.asList(
             new BaseBall(12, "Demix", Color.BLACK),
-            new FootBall(7, "Anta", Color.GREEN)
-        }));
+            new FootBall(7, "Anta", Color.GREEN));
 
         var expected = new ArrayList<>(actual);
 
@@ -125,13 +122,12 @@ class SortServiceTest {
     @ParameterizedTest
     @MethodSource("sortServiceStream")
     void sameSizeElementArrayTest(SortService sortService) {
-        var actual = new ArrayList<>(List.of(new Ball[]{
+        var actual = Arrays.asList(
             new BaseBall(2, "Demix", Color.BLACK),
             new FootBall(2, "Anta", Color.GREEN),
             new BasketBall(2, "Nike", Color.RED),
             new FootBall(2, "Nike", Color.GREEN),
-            new BaseBall(2, "Adidas", Color.BLUE)
-        }));
+            new BaseBall(2, "Adidas", Color.BLUE));
 
         var expected = new ArrayList<>(actual);
 
