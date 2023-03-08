f80aa62f8c61262c4cb16317c45635962c9ba255
evensky1 <fromthedeepsize@gmail.com>
2023-03-08 16:01:51 +0300

    OddEvenSort refactoring

--- src/main/java/com/innowise/internship/sort/impl/OddEvenSortServiceImpl.java
+++ src/main/java/com/innowise/internship/sort/impl/OddEvenSortServiceImpl.java
@@ -2,10 +2,9 @@ package com.innowise.internship.sort.impl;
 
 import com.innowise.internship.model.Ball;
 import com.innowise.internship.sort.SortService;
-import java.util.Collections;
+import com.innowise.internship.sort.util.ExchangeTask;
 import java.util.Comparator;
 import java.util.List;
-import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -15,15 +14,17 @@ public class OddEvenSortServiceImpl implements SortService {
     @Override
     public void sort(List<Ball> ballList, Comparator<Ball> comparator) {
 
-        if (ballList.size() <= 1) return;
+        if (ballList.size() <= 1) {
+            return;
+        }
 
         var threadCount = ballList.size() / 2;
         var threadPool = Executors.newFixedThreadPool(threadCount);
         var cyclicBarrier = new CyclicBarrier(threadCount);
 
         for (int i = 0; i < threadCount; i++) {
-            threadPool.submit(
-                new CompareAndExchangeTask(ballList, comparator, 2 * i + 1, cyclicBarrier));
+            threadPool.execute(
+                new ExchangeTask(ballList, comparator, 2 * i + 1, cyclicBarrier));
         }
 
         try {
@@ -31,56 +32,6 @@ public class OddEvenSortServiceImpl implements SortService {
             threadPool.awaitTermination(1, TimeUnit.DAYS);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
-        }
-    }
-}
-
-class CompareAndExchangeTask implements Runnable {
-
-    private final List<Ball> ballList;
-    private final Comparator<Ball> comparator;
-    private final int threadPos;
-    private final CyclicBarrier cyclicBarrier;
-
-    public CompareAndExchangeTask(
-        List<Ball> ballList,
-        Comparator<Ball> comparator,
-        int threadPos,
-        CyclicBarrier cyclicBarrier
-    ) {
-        this.ballList = ballList;
-        this.comparator = comparator;
-        this.threadPos = threadPos;
-        this.cyclicBarrier = cyclicBarrier;
-    }
-
-    @Override
-    public void run() {
-        for (int i = 0; i < ballList.size(); i++) {
-            compareAndSwap(threadPos - 1, threadPos);
-
-            waitForOthers();
-
-            if (threadPos + 1 < ballList.size()) {
-                compareAndSwap(threadPos, threadPos + 1);
-            }
-
-            waitForOthers();
-        }
-    }
-
-    private void waitForOthers() {
-        try {
-            cyclicBarrier.await();
-        } catch (InterruptedException | BrokenBarrierException e) {
-            e.printStackTrace();
-            Thread.currentThread().interrupt();
-        }
-    }
-
-    private void compareAndSwap(int i, int j) {
-        if (comparator.compare(ballList.get(i), ballList.get(j)) > 0) {
-            Collections.swap(ballList, i, j);
         }
     }
 }
---
+++ src/main/java/com/innowise/internship/sort/util/ExchangeTask.java
@@ -0,0 +1,58 @@
+package com.innowise.internship.sort.util;
+
+import com.innowise.internship.model.Ball;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+public class ExchangeTask implements Runnable {
+
+    private final List<Ball> ballList;
+    private final Comparator<Ball> comparator;
+    private final int threadPos;
+    private final CyclicBarrier cyclicBarrier;
+
+    public ExchangeTask(
+        List<Ball> ballList,
+        Comparator<Ball> comparator,
+        int threadPos,
+        CyclicBarrier cyclicBarrier
+    ) {
+        this.ballList = ballList;
+        this.comparator = comparator;
+        this.threadPos = threadPos;
+        this.cyclicBarrier = cyclicBarrier;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < ballList.size(); i++) {
+            compareAndSwap(threadPos - 1, threadPos);
+
+            waitForOtherThreads();
+
+            if (threadPos + 1 < ballList.size()) {
+                compareAndSwap(threadPos, threadPos + 1);
+            }
+
+            waitForOtherThreads();
+        }
+    }
+
+    private void waitForOtherThreads() {
+        try {
+            cyclicBarrier.await();
+        } catch (InterruptedException | BrokenBarrierException e) {
+            e.printStackTrace();
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private void compareAndSwap(int i, int j) {
+        if (comparator.compare(ballList.get(i), ballList.get(j)) > 0) {
+            Collections.swap(ballList, i, j);
+        }
+    }
+}
